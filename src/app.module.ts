eaa464d2f66cebc42ebbfd3ebd2767241093bed8
eddie wang <wang8119@gmail.com>
2024-11-19 18:38:06 +0800

    feat: synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,9 @@ import { Report } from './reports/report.entity';
       database: 'db.sqlite',
       entities: [User, Report],
       synchronize: true,
+      // synchronize:
+      // 同步功能僅適用於開發環境 可以節省工 會自動更新資料庫的結構 就不用到migration
+      // 永遠不要在生產中運行 true 的遷移或同步功能 ->容易意外地說刪除列 要改用migration
     }),
     UsersModule,
     ReportsModule,
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,8 +1,10 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+// @Entity() 裝飾器是 TypeORM 進行 ORM 映射的基礎，將類別與資料庫結構對應起來。
+// 類別映射到資料庫中的表，可以以物件導向的方式操作資料庫
 @Entity()
 export class User {
-  //注意這取名規範
+  //注意User取名規範
   @PrimaryGeneratedColumn()
   id: number;
 
