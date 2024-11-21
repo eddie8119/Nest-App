71bf934e3504a5dce69f2a8f0b857fcb7e258031
eddie wang <wang8119@gmail.com>
2024-11-21 12:37:32 +0800

    feat: Finding and Filtering Records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
   @Post('signup')
   createUser(@Body() body: CreateUserDto) {
     const { email, password } = body;
     this.usersService.create(email, password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUser(@Query('email') email: string) {
+    return this.usersService.find(email);
   }
 }
